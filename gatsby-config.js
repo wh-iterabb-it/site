cf6f022d56a2817dd4fdd775b8140af959d61deb
beaubouchard <beau@beaubouchard.com>
2019-10-12 12:07:31 -0400

    feat: moved the favicons->icons helmet should set viewport correctly, icons should now connect to helmet maps

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon/favicon-96x96.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/favicon-96x96.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
