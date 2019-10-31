58e666d8521b7870bf8a96ce6aacd2bf725bcb77
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2019-10-30 21:39:55 -0400

    feat: added tiny letter (#27)

---
+++ src/components/tinyletter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TinyLetter = () => (
+  <form
+    action="https://tinyletter.com/wh-iterabb-it"
+    method="post"
+    target="popupwindow"
+    onsubmit="window.open('https://tinyletter.com/wh-iterabb-it', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+    <p><label for="tlemail">Enter your email address to get our monthly zine</label></p>
+    <p><input type="text"  name="email" id="tlemail" /></p>
+    <input type="hidden" value="1" name="embed"/>
+    <input type="submit" value="Subscribe" />
+  </form>
+);
+
+
+export default TinyLetter
