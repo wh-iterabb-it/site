174e61413ce8e5a20aa0248fff67c317f221a9e0
beaubouchard <bnb2011@gmail.com>
2023-05-16 19:18:58 -0400

    feat: deps installed with 18

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
 			resolve: 'gatsby-plugin-google-fonts',
